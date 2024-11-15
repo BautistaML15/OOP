1bf88f439df12bba503ec446e6f7670552a8b1e7
BautistaML15 <23-13461@g.batstate-u.edu.ph>
2024-11-15 17:23:33 +0800

    Add files via upload

---
+++ InheritanceSample.java
@@ -0,0 +1,18 @@
+public class InheritanceSample {
+    public static void main(String[] args) {
+
+        Student student1 = new Student ("Juan", "Batangas PH", "BSIT", 2);
+        System.out.println("Student Name   : " + student1.getName());
+        System.out.println("Student Address: " + student1.getAddress());
+        System.out.println("Student Program: " + student1.getProgram());
+        System.out.println("Student Year   : " + student1.getYear());
+        System.out.println();
+
+        Staff staff1 = new Staff ("Pedro", "Batangas PH", "Batangas State University", 1500.00);
+        System.out.println("Staff Name   : " + staff1.getName());
+        System.out.println("Staff Address: " + staff1.getAddress());
+        System.out.println("Staff School : " + staff1.getSchool());
+        System.out.println("Staff Salary : " + staff1.getSalary());
+
+    }
+}
